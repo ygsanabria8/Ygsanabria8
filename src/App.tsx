7c0e7c26b850f8ed977fdaf8b6a8e062d10794cb
Yesid S. <63482247+Ygsanabria8@users.noreply.github.com>
2023-12-29 22:23:23 -0500

    feat: Add basic navbar with menu options

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { PrimeReactProvider } from 'primereact/api';
 
+import { Navbar } from './ui/sections/Navbar/Navbar';
+
 import '/node_modules/primeflex/primeflex.css';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 export const App = () => {
   return (
     <PrimeReactProvider>
-      <p>Yesid Sanabria</p>
+      <Navbar/>
     </PrimeReactProvider>
   )
 }
