89d14503c2995c990c0530d563d275f9999403c3
Yesid S. <63482247+Ygsanabria8@users.noreply.github.com>
2024-01-02 21:26:00 -0500

    feat: Add hero component into web

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Navbar } from './ui/sections/Navbar/Navbar';
 
 import '/node_modules/primeflex/primeflex.css';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { Hero } from './ui/sections/Hero/Hero';
 
 export const App = () => {
   return (
     <PrimeReactProvider>
       <Navbar/>
+      <Hero/>
     </PrimeReactProvider>
   )
 }
