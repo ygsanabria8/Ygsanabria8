e9e03fa7057d94beb9a81c9e1d9c8a232b44b73d
Yesid S. <63482247+Ygsanabria8@users.noreply.github.com>
2024-01-27 21:32:29 -0500

    feature change about legend

--- src/ui/sections/About/About.tsx
+++ src/ui/sections/About/About.tsx
@@ -10,15 +10,19 @@ export const About = () => {
                     <p className="text-4xl weight-semi-bold">Sobre mí</p>
                 </div>
                 <p className="mt-3 text-xl col-12 lg:col-6">
-                    Apasionado por el desarrollo y diseño de software. 
-                    He trabajado en proyectos de desarrollo ágil implementando soluciones a 
-                    la medida con altos estándares de calidad y tecnologías a la vanguardia.
+                    Ingeniero en Sistemas y Computación con una trayectoria de más de 3 años especializado en el 
+                    desarrollo FullStack y liderazgo técnico en proyectos de aplicaciones móviles y web. 
+                    Mi experiencia se centra en el mantenimiento, la mejora continua y el desarrollo de nuevas 
+                    funcionalidades en entornos ágiles, donde implemento soluciones a la medida con altos estándares 
+                    de calidad y utilizando tecnologías de vanguardia.
                     <br/>
+                    Mi sólido conjunto de habilidades técnicas me ha permitido alcanzar con éxito los objetivos 
+                    establecidos en los proyectos en los que he participado. He demostrado liderazgo, habilidades
+                     de comunicación efectiva, capacidad para trabajar en equipo, así como una actitud proactiva 
+                     y orientada a la resolución de problemas.
                     <br/>
-                    Estoy en constante aprendizaje para poder llevar al máximo mis habilidades 
-                    al desarrollo e implementación de nuevas tecnologías. 
-                    Cuento con habilidades como lo son pensamiento crítico, 
-                    comunicación asertiva, autogestión y liderazgo.
+                    Estoy comprometido con la excelencia técnica y el aprendizaje continuo, siempre buscando innovar 
+                    y encontrar soluciones eficientes que impulsen el éxito de los proyectos en los que participo.
                 </p>
             </section>
         </article>
