831fae2b847c666546b3e7dd95e53df2ac2dbbf2
Yesid S. <63482247+Ygsanabria8@users.noreply.github.com>
2024-01-29 20:28:42 -0500

    Feature add footer and scrolltop button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { PrimeReactProvider } from 'primereact/api';
 
 import { Navbar } from './ui/sections/Navbar/Navbar';
 
+import { ScrollTop } from 'primereact/scrolltop';
+
 import 'primereact/resources/themes/lara-light-green/theme.css';
 import 'primeflex/primeflex.css';
 
@@ -9,6 +11,7 @@ import { Hero } from './ui/sections/Hero/Hero';
 import { About } from './ui/sections/About/About';
 import { Technologies } from './ui/sections/Technologies/Technologies';
 import { Experience } from './ui/sections/Experience/Experience';
+import { Footer } from './ui/sections/Footer/Footer';
 
 export const App = () => {
   return (
@@ -18,6 +21,8 @@ export const App = () => {
       <About/>
       <Technologies/>
       <Experience/>
+      <Footer/>
+      <ScrollTop />
     </PrimeReactProvider>
   )
 }
---
+++ src/ui/sections/Footer/Footer.tsx
@@ -0,0 +1,47 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+export const Footer = () => {
+    return (
+        <article className="mt-5">
+            <section className="container">
+                <div className='contact grid grid-nogutter gap-3 mt-2 justify-content-center'>
+                    <a href="https://github.com/Ygsanabria8" target='_blank'>
+                        <FontAwesomeIcon
+                            icon={faGithub}
+                            size='lg'
+                            className='cursor-pointer'
+                        />
+                    </a>
+                    <a href="https://www.linkedin.com/in/yesid-sanabria" target='_blank'>
+                        <FontAwesomeIcon
+                            icon={faLinkedin}
+                            size='lg'
+                            className='cursor-pointer'
+                        />
+                    </a>
+                    <a href="mailto:yesid2571@hotmail.com">
+                        <FontAwesomeIcon
+                            icon={faEnvelope}
+                            size='lg'
+                            className='cursor-pointer'
+                        />
+                    </a>
+                    <a href="tel:+573144651658">
+                        <FontAwesomeIcon
+                            icon={faPhone}
+                            size='lg'
+                            className='cursor-pointer'
+                        />
+                    </a>
+                </div>
+                <p className="text-sm col-12 text-center">
+                    Copyright © | Coded by Yesid Sanabria
+                </p>
+            </section>
+        </article>
+    )
+}
